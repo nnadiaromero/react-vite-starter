e3d7348753db133ce76ccf43a60b799f88d73755
Your Name <you@example.com>
2024-06-01 00:03:57 +0200

    card sin terminar

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,29 @@ function App() {
     <main>
       <section className="cards">
         <article className="bulbasaur">
-          <title className="titles">
-          <h2 className="name">Bulbasaur</h2>
-          <h3 className="numero">#001</h3>
-          </title>
           <img className="imgbulbasaur" src="bulbasaur.png" />
+
+          <label className="titles">
+            <h2 className="name">Bulbasaur</h2>
+            <h3 className="numero">#001</h3>
+          </label>
           <div className="features">
-          <div className="natureFeatures">
-            <label>Grass</label>
-            <label>Poison</label>
-          </div>
-          <div className="physicalFeatures">
-            <label>6.9 hg</label>
-            <label>0.7 m</label>
-          </div>
-          <ul className="lista">
-            <li>HP</li>
-            <li>ATK</li>
-            <li>DEF</li>
-            <li>SAT</li>
-            <li>SDF</li>
-            <li>SPD</li>
-          </ul>
+            <div className="natureFeatures">
+              <label>Grass</label>
+              <label>Poison</label>
+            </div>
+            <div className="physicalFeatures">
+              <label>6.9 hg</label>
+              <label>0.7 m</label>
+            </div>
+            <ul className="lista">
+              <li>HP</li>
+              <li>ATK</li>
+              <li>DEF</li>
+              <li>SAT</li>
+              <li>SDF</li>
+              <li>SPD</li>
+            </ul>
           </div>
         </article>
       </section>
