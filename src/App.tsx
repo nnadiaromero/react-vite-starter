69c7f2adb00309aee02b77d6b7d91920ccba1179
Your Name <you@example.com>
2024-06-01 12:25:51 +0200

    card features proceso

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,28 @@ function App() {
     <main>
       <section className="cards">
         <article className="bulbasaur">
-          {/* <img className="imgbulbasaur" src="bulbasaur.png" /> */}
-
           <label className="titles">
             <h2 className="name">Bulbasaur</h2>
             <h3 className="numero">#001</h3>
           </label>
           <div className="features">
-            <div className="natureFeatures">
-              <label className="grass">Grass</label>
-              <label className="poison">Poison</label>
+            <img className="imgbulbasaur" src="bulbasaur.png" />
+
+            <div className="tags">
+              <label className="tag grass">
+                <img className="tagIcon" src="grass.svg" />
+                Grass
+              </label>
+              <label className="tag poison">
+                <img className="tagIcon" src="poison.svg" />
+                Poison
+              </label>
             </div>
+
             <div className="physicalFeatures">
-              <label>6.9 hg</label>
-              <label>0.7 m</label>
+              <label><img className="weightIcon" src="./public/weight.svg"/> 6.9 kg</label>
+              <div className="line"></div>
+              <label><img className="rulerIcon" src="./public/ruler.svg"/>0.7 m</label>
             </div>
             <ul className="lista">
               <li>HP</li>
