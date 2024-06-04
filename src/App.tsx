ece1e9767dbabd138213e95494830b801b13000a
Your Name <you@example.com>
2024-06-04 19:42:57 +0200

    card exported

--- src/App.tsx
+++ src/App.tsx
@@ -3,50 +3,62 @@ type modelo = {
   name: string
   number: string
   image: string
-  stats:  Array<{slug:string; value: number}>
-  tags: string[]
+  tags: { tagCharacter: string; tagWeapon: string }
+  physicalFeatures: {weight: string; measure: string}
+  stats: Array<{ slug: string; value: string }>
+
 }
 const pokemon: modelo = {
   name: 'Bulbasaur',
-  number: "#001",
+  number: '#001',
   image:
     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  tags: {
+    tagCharacter: "Grass",
+    tagWeapon: "Poison"
+  },
+  physicalFeatures: {
+    weight:"6.9 kg",
+    measure: "0.7 m"
+  },
   stats: [
     {
       slug: 'health-points',
-      value: 45,
+      value: '45',
     },
     {
       slug: 'attack',
-      value: 45,
+      value: '45',
     },
     {
       slug: 'defense',
-      value: 45,
+      value: '45',
     },
     {
       slug: 'sat',
-      value: 45,
+      value: '45',
     },
     {
       slug: 'sdf',
-      value: 45,
+      value: '45',
     },
     {
       slug: 'speed',
-      value: 45,
+      value: '45',
     },
   ],
-  tags: []
 }
 
-type statsLista ={
-  [key:string]: string
+type statsLista = {
+  [key: string]: string
 }
 const statName: statsLista = {
   'health-points': 'HP',
-  'attack': "ATK",
-  
+  "attack": 'ATK',
+  "defense": 'DEF',
+  "sat": 'SAT',
+  "sdf": 'SDF',
+  "speed": 'SPD',
 }
 
 function App() {
@@ -64,22 +76,23 @@ function App() {
             <div className="tags">
               <label className="tag grass">
                 <img className="tagIcon" src="grass.svg" />
-                Grass
+                {pokemon.tags.tagCharacter}
               </label>
               <label className="tag poison">
                 <img className="tagIcon" src="poison.svg" />
-                Poison
+                {pokemon.tags.tagWeapon}
               </label>
             </div>
 
             <div className="physicalFeatures">
               <label className="tagPhysical">
-                <img className="weightIcon" src="./public/weight.svg" /> 6.9 kg
+                <img className="weightIcon" src="./public/weight.svg" /> 
+                {pokemon.physicalFeatures.weight}
               </label>
               <div className="line"></div>
               <label className="tagPhysical">
                 <img className="rulerIcon" src="./public/ruler.svg" />
-                0.7 m
+                {pokemon.physicalFeatures.measure}
               </label>
 
               {/*BARRAS DE PROGRESO*/}
@@ -90,42 +103,52 @@ function App() {
                   <label className="item">
                     {statName[pokemon.stats[0].slug]}
                   </label>
-                  <label>{statName[pokemon.stats[0].value]}</label>
+                  <label>{pokemon.stats[0].value.padStart(3, '0')}</label>
                 </label>
                 <progress className="progress" max="250" value="45"></progress>
               </li>
               <li className="itemList">
                 <label className="itemInfo">
-                  <label className="item">{statName[pokemon.stats[1].slug]}</label>
-                  <label>045</label>
+                  <label className="item">
+                    {statName[pokemon.stats[1].slug]}
+                  </label>
+                  <label>{pokemon.stats[1].value.padStart(3, '0')}</label>
                 </label>
                 <progress className="progress" max="250" value="45"></progress>
               </li>
               <li className="itemList">
                 <label className="itemInfo">
-                  <label className="item">DEF</label>
-                  <label>045</label>
+                  <label className="item">
+                    {statName[pokemon.stats[2].slug]}
+                  </label>
+                  <label>{pokemon.stats[2].value.padStart(3, '0')}</label>
                 </label>
                 <progress className="progress" max="250" value="45"></progress>
               </li>
               <li className="itemList">
                 <label className="itemInfo">
-                  <label className="item">SAT</label>
-                  <label>045</label>
+                  <label className="item">
+                    {statName[pokemon.stats[3].slug]}
+                  </label>
+                  <label>{pokemon.stats[3].value.padStart(3, '0')}</label>
                 </label>
                 <progress className="progress" max="250" value="45"></progress>
               </li>
               <li className="itemList">
                 <label className="itemInfo">
-                  <label className="item">SDF</label>
-                  <label>045</label>
+                  <label className="item">
+                    {statName[pokemon.stats[4].slug]}
+                  </label>
+                  <label>{pokemon.stats[4].value.padStart(3, '0')}</label>
                 </label>
                 <progress className="progress" max="250" value="45"></progress>
               </li>
               <li className="itemList">
                 <label className="itemInfo">
-                  <label className="item">SPD</label>
-                  <label>045</label>
+                  <label className="item">
+                    {statName[pokemon.stats[5].slug]}
+                  </label>
+                  <label>{pokemon.stats[5].value.padStart(3, '0')}</label>
                 </label>
                 <progress className="progress" max="250" value="45"></progress>
               </li>
@@ -136,5 +159,6 @@ function App() {
     </main>
   )
 }
+
 
 export default App
