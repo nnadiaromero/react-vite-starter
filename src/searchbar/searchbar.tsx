c189f6ef118ab2928f69d07d034314f4e472b753
Your Name <you@example.com>
2024-06-07 19:39:14 +0200

    header and searchbar done

--- src/searchbar/searchbar.tsx
+++ src/searchbar/searchbar.tsx
@@ -1,12 +1,18 @@
-import "./searchbar.modules.css"
+import './searchbar.modules.css'
 
 export function SearchBar() {
   return (
     <div className="buscador">
-      <input className="barra" type="text" placeholder="Search a Pokémon..."></input>
-      <img className="lupa" src="./public/Magnifying glass.svg" />
+      <section className="bar">
+        <img className="lupa" src="./public/Magnifying glass.svg" />
 
-      <input hidden type="submit"></input>
+        <input
+          className="text"
+          type="text"
+          placeholder="Search a Pokémon..."
+        ></input>
+        {/* <input hidden type="submit"></input> */}
+      </section>
     </div>
   )
 }
